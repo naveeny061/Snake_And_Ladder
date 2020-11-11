2a2f4165636a60e34b1028b31caa5f56dc2eaa3b
naveeny061 <naveeny061@gmail.com>
2020-11-11 13:33:38 +0530

    Add: Starting with position 0

--- snakeAndLadderSimulator.java
+++ snakeAndLadderSimulator.java
@@ -1,7 +1,14 @@
 public class snakeAndLadderSimulator {
+	void start() {
+		int start = 0;
+		System.out.println("Starting position=" + start);
+	}
 
 	public static void main(String[] args) {
+		snakeAndLadderSimulator game = new snakeAndLadderSimulator();
+		
 		System.out.println("Welcome to Snake & Ladder Simulator");
+		game.start();
 
 	}
 
