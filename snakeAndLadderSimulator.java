6301771c87ebc3e9a5753e6a2e16bf88099a93e1
naveeny061 <naveeny061@gmail.com>
2020-11-11 14:17:46 +0530

    Add: Function to make moves

--- snakeAndLadderSimulator.java
+++ snakeAndLadderSimulator.java
@@ -2,25 +2,49 @@ import java.util.Random;
 
 public class snakeAndLadderSimulator {
 	// UC1 starting with positio 0
-	void start() {
+	int start() {
 		int start = 0;
 		System.out.println("Starting position=" + start);
+		return start;
 	}
 
 	// UC2 roll the dice
-	void rollDice() {
-		Random roll = new Random();
-		int dice = roll.nextInt(6) + 1;
+	int rollDice() {
+		Random random = new Random();
+		int dice = random.nextInt(6) + 1;
 		System.out.println("Dice=" + dice);
+		return dice;
+	}
+
+	// UC3 Playing the game by making choice
+	void play(int position, int dice) {
+		Random random = new Random();
+		int choice = random.nextInt(3);
+		switch (choice) {
+		case 0:
+			System.out.println("No play ---- position = " + position);
+			break;
+		case 1:
+			position += dice;
+			System.out.println("Ladder ---- position = " + position);
+			break;
+		case 2:
+			position -= dice;
+			System.out.println("Snake ---- position = " + position);
+
+		}
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake & Ladder Simulator");
 		snakeAndLadderSimulator game = new snakeAndLadderSimulator();
-		game.start();
-		game.rollDice();
+
+		int position = game.start();
+		int dice = game.rollDice();
+		game.play(position, dice);
 
 	}
 
 }
+
 
