52c8dddc9ba7297b6685feafbabe94b7249aaae7
naveeny061 <naveeny061@gmail.com>
2020-11-11 13:46:54 +0530

    Add: Function to roll the dice

--- snakeAndLadderSimulator.java
+++ snakeAndLadderSimulator.java
@@ -1,15 +1,26 @@
+import java.util.Random;
+
 public class snakeAndLadderSimulator {
+	// UC1 starting with positio 0
 	void start() {
 		int start = 0;
 		System.out.println("Starting position=" + start);
 	}
 
+	// UC2 roll the dice
+	void rollDice() {
+		Random roll = new Random();
+		int dice = roll.nextInt(6) + 1;
+		System.out.println("Dice=" + dice);
+	}
+
 	public static void main(String[] args) {
-		snakeAndLadderSimulator game = new snakeAndLadderSimulator();
-		
 		System.out.println("Welcome to Snake & Ladder Simulator");
+		snakeAndLadderSimulator game = new snakeAndLadderSimulator();
 		game.start();
+		game.rollDice();
 
 	}
 
 }
+
